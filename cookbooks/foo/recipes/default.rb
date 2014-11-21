c8fa1e0688af69772036a80e89f9b9660b1135f3
Man Chun Lam <manchunlam@gmail.com>
2014-11-21 18:12:35 -0500

    [CHANGE] berkshelf - have chef install berkshelf

--- cookbooks/foo/recipes/default.rb
+++ cookbooks/foo/recipes/default.rb
@@ -7,14 +7,4 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe 'rbenv::default'
-include_recipe 'rbenv::ruby_build'
-
-rbenv_ruby '1.9.3-p551' do
-  ruby_version '1.9.3-p551'
-  global true
-end
-
-rbenv_gem 'bundler' do
-  ruby_version '1.9.3-p551'
-end
+include_recipe "berkshelf"
