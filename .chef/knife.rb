68674ba2a434ee4e1a9f04d412daf86ea824fe60
ManChun Lam <manchunlam@gmail.com>
2015-01-07 17:36:02 -0500

    [CHANGE] Fresh Start - start from scratch

--- .chef/knife.rb
+++
@@ -1,5 +0,0 @@
-# knife.rb
-
-ssl_verify_mode :verify_peer
-
-cookbook_path ['/root/chef-repo/cookbooks']
--- cookbooks/foo/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'foo'
-maintainer       'Joe Lam'
-maintainer_email 'manchunlam@gmail.com'
-license          'All rights reserved'
-description      'Installs/Configures foo'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
-depends 'berkshelf'
